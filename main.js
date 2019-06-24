6593ef54e0453f5aae0a844a018422ba1f861966
yasuya <porepore1996@gmail.com>
2019-06-24 18:03:35 +0900

    down判定の製作途中

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
+var winWidth = 700;
+var winHeight = 600;
+
 var c1 = document.getElementById("canvas1");
 var base = c1.getContext("2d");
 var c2 = document.getElementById("canvas2");
 var main = c2.getContext("2d");
 var c3 = document.getElementById("canvas3");
-var surf = c3.getContext("2d");
+var eff1 = c3.getContext("2d");
+var c4 = document.getElementById("canvas3");
+var eff2 = c4.getContext("2d");
+
+var downX,downY,upX,upY;
+var downFlag = 0;//0:デフォルト 1:原子 2:結合 3:空
+var moveFlag = 0;
+var upFlag = 0;
 
 window.onload = function(){
     background();
     setButton();
 };
 
-c3.onclick = function(click){
-    var clickX = click.offsetX;
-    var clickY = click.offsetY;
+c4.onmousedown = function(down){
+    downX = down.offsetX;
+    downY = down.offsetY;
     if(status == 0){
-        if(ture){
-            newAtom(clickX,clickY,12);
+        atoms.forEach(function(arrey){
+            //ここに円判定
+        });
 
-        }
+    }
+};
+c4.onmouseup = function(up){
+    upX = up.offsetX;
+    upY = up.offsetY;
+    switch(downFlag){
+        case 0:
+            break;
+        case 1:
+            var first = newAtom(downX,downY,12);
+            var second = newAtom(downX+20*Math.sqrt(3),downY-20,12);
+            newBond(first,second);
+            break;
     }
 };
 
@@ -34,16 +57,34 @@ function setButton(){
     base.fillRect(0,170,50,50);
 }
 function newAtom(x,y,num){
-    var q = atom.length;
-    atom.push([q,x,y,num]);
+    var q = atoms.length;
+    atoms.push([q,x,y,num]);
+    return q;
 }
 function newBond(a,b){
     var q = bond.length;
-    var x = (atom[a][1]+atom[b][1])/2;
-    var y = (atom[a][2]+atom[b][2])/2;
+    var x = (atoms[a][1]+atoms[b][1])/2;
+    var y = (atoms[a][2]+atoms[b][2])/2;
+    main.beginPath();
+    main.lineWidth = 2;
+    main.moveTo(atoms[a][1],atoms[a][2]);
+    main.lineTo(atoms[b][1],atoms[b][2]);
+    main.stroke();
     bond.push(q,x,y,a,b,1);
 }
-function defaultSet(){
-    var a = 1;
+function clear(layer){
+    layer.clearRect(0,0,winWidth,winHeight);
+}
+function atomMark(id){
+    eff1.beginPath();
+    eff1.strokeStyle = "rgba(0,0,100,0.5)";
+    eff1.arc(atoms[id][1],atoms[id][2],8,0,Math.PI*2,true);
+    eff1.fill();
+}
+function ring(x,y){
+    eff1.beginPath();
+    eff1.strokeStyle = "rgba(0,0,100,0.5)";
+    eff1.arc(x,y,12,0,Math.PI*2,true);
+    eff1.stroke();
 }
 
